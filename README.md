cf8bf49a72f5f44749b219159258856af423e69b
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2017-02-02 01:58:42 -0800

    improved tasks, fixed double notification bug, updated readme

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 1. Make sure you have NodeJS and NPM Updated/Installed [https://nodejs.org/en/](https://nodejs.org/en/)
 2. Make sure the `ruby sass gem` is installed [http://sass-lang.com/install](http://sass-lang.com/install)
-3. Install gulp globally `npm install -g gulp` 
+3. Install gulp globally `npm install -g gulp`
 
 ## Setup
 
 1. Download the repo or make sure to update/create your `package.json` file and if you are using Bower, your `bower.json` file.
 2. I also included a `.babelrc` file (make sure you have this file), which is used by the "babel" npm package, which is needed for compiling JavaScript written in ES2015.
-	- You will need this if you are using Foundation 6.2 or later (or if you are using any JavaScript written in ES2015) 
+	- You will need this if you are using Foundation 6.2 or later (or if you are using any JavaScript written in ES2015)
 	- If you are not using either of these, there is nothing to worry about, your JavaScript will compile just the same.
 3. Install project dependencies. Navigate to project (in terminal) and run `npm install` and `bower install`
 	- This will install any dependences in your `package.json` and `bower.json`.
@@ -17,38 +17,38 @@ Everything is now ready to use.
 
 ## Gulp Tasks
 
-Below are the tasks that I have included in the `gulpfile.js` and what they do.
+Below are the tasks that are included in the `gulpfile.js` and what they do.
 
 ### Gulp styles
 
-Complies your `.scss` files and uses autoprefixer, so there is no need to use any prefixes.
-	- currently set to `browsers: ['last 2 versions', 'ie >= 9']`, change as need in `gulpfile.js`
+Complies `styles.scss` and uses autoprefixer, so there is no need to use any prefixes in your source files.
+	- Currently set to `browsers: ['last 2 versions', 'ie >= 9']`, change as need in `gulpfile.js`
+	- To compile a different file, update `gulpfile.js`
+	- If you are not using `Zurb's Foundation` with bower, remove `bower_components/foundation-sites/scss` from `sassPaths` array in `gulpfile.js`
+		- Additionally if you are referencing any `scss` files inside of `styles.scss` located outside of `src` folder or other `bower_components/*` scss files, it may be easier to include the path in `sassPaths` array.
 
-Saves to `assets/css`
+This task also includes code to compile a `iestyle.css` if you need it.
+	- This is commented out by default, uncomment to include in this task
+
+Saves to `assets/css/styles.css`
+Saves to `assets/css/iestyles.css`
 
 To use, run `gulp styles`
 
-### Gulp iestyles
-
-Complies an iestyles.css
-	- sames as above
-
-Saves to `assets/css`
-
-To use, run `gulp iestyles`	
 
 ### Gulp scripts
 
-Minifies JavaScript and runs through `babel` to compile any `ES2015` JavaScript you may use.
+Runs through `js-hint`, then runs through `babel` (to compile any `ES2015` JavaScript you may use), then minifies your JavaScript
 
-Saves to `assets/js`
+Saves to `assets/js/[file-name].min.js`
 
-To use, run `gulp scripts`	
+To use, run `gulp scripts`
 
 ### Gulp bower-files
 
-Goes through and complies/minify your bower components and moves them to a `lib` folder inside of `assets`
-	- most likely you will need to add "overrides" in your bower.json (see the bower.json in this repo for an example)
+Goes through and complies/minify your bower components  `.js` files and moves them to a `assets/lib`
+	- This will also filter any `.png`, `.jpg`, `.gif` inside of the `assets/img/[package-name]` folder
+	- Most likely you will need to add "overrides" in your bower.json (see the bower.json in this repo for an example)
 
 To use, run `gulp bower-files`
 
@@ -62,9 +62,10 @@ To use, run `gulp images`
 ### Gulp watch
 
 Creates a Proxy and will run the tasks to compile/minify your scss, js and compress your images.
-	- you will need to update the proxy url in you `gulpfiles.js`
+	- You will need to update the proxy url in your `gulpfiles.js`
 
-Also will reload the page when you change `.php` or `.html` files  
+This task is configured to reload the page when you change `.php` or `.html` files
+	- Update as needed in `gulpfile.js`
 
 To use, run `gulp watch`
 
@@ -79,4 +80,4 @@ To use, run `gulp clean`
 
 Runs `gulp styles`, `gulp scripts`, `gulp images`, and `gulp bower-files`
 
-To use, run `gulp`	
+To use, run `gulp`
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,34 @@
-var gulp = require('gulp'),
+var autoprefixer = require('gulp-autoprefixer'),
+    babel = require('gulp-babel'),
+    browserSync = require('browser-sync').create(),
+    cache = require('gulp-cache'),
+    concat = require('gulp-concat'),
+    del = require('del'),
+    gulp = require('gulp'),
+    gulpFilter = require('gulp-filter'),
+    imagemin = require('gulp-imagemin'),
+    jshint = require('gulp-jshint'),
+    mainBowerFiles = require('gulp-main-bower-files'),
+    minifycss = require('gulp-cssnano'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber');
+    rename = require('gulp-rename'),
     sass = require('gulp-ruby-sass'),
     sourcemaps = require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-cssnano'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    browserSync = require('browser-sync').create(),
-    del = require('del'),
-    mainBowerFiles = require('gulp-main-bower-files'),
-    babel = require('gulp-babel'),
-    gulpFilter = require('gulp-filter'),
-    util = require('gulp-util'),
     stylish = require('jshint-stylish'),
-    plumber = require('gulp-plumber');
+    uglify = require('gulp-uglify'),
+    util = require('gulp-util');
 
 var sassPaths = [
-  'bower_components/foundation-sites/scss' //update as needed
+  'bower_components/foundation-sites/scss' //update as needed, remove if not using
 ];
 
 // error handler to prevent watch task from crashing // https://andidittrich.de/2016/03/prevent-errors-from-breaking-gulp-watch.html
 var errorHandler = function(){
     return plumber(function(error){
         // add indentation
-        var msg = error.codeFrame.replace(/\n/g, '\n    ');
+        //var msg = error.codeFrame.replace(/\n/g, '\n    '); // causing issues ... need to look into
+        var msg = '';
         // output styling
         util.log('|- ' + util.colors.bgRed.bold('Build Error in ' + error.plugin));
         util.log('|- ' + util.colors.bgRed.bold(error.message));
@@ -37,34 +38,55 @@ var errorHandler = function(){
     });
 };
 
-
-gulp.task('styles', function() {
-    return sass('src/css/scss/styles.scss', { style: 'compressed', sourcemap: true, loadPath: sassPaths  }) // expanded compressed
-        .pipe(errorHandler())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'ie >= 9']
-        }))
-        .pipe(sourcemaps.write('maps', {
-          includeContent: false,
-          sourceRoot: 'source'
-        }))
-        .pipe(gulp.dest('assets/css'))
-        .pipe(notify({ message: 'Styles task complete' }))
-        .pipe(browserSync.stream({match: '**/*.css'}));
+gulp.task('pre-clean-styles', function() {
+    return del('assets/css');
 });
 
-gulp.task('iestyles', function() {
-    return sass('src/css/scss/iestyles.scss', { style: 'compressed', sourcemap: true, loadPath: sassPaths  }) // expanded compressed
-        .pipe(errorHandler())
-        .pipe(sourcemaps.write('maps', {
-          includeContent: false,
-          sourceRoot: 'source'
-        }))
-        .pipe(gulp.dest('assets/css'))
-        .pipe(notify({ message: 'IE Styles task complete' }))
+gulp.task('styles', ['pre-clean-styles'], function() {
+    return [
+        sass('src/css/scss/styles.scss', {
+                style: 'compressed', // expanded compressed
+                sourcemap: true,
+                loadPath: sassPaths
+            })
+            .pipe(errorHandler())
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions', 'ie >= 9']
+            }))
+            .pipe(sourcemaps.write('maps', {
+                includeContent: false,
+                sourceRoot: 'source'
+            }))
+            .pipe(gulp.dest('assets/css'))
+            .pipe(notify({
+                message: 'Styles task complete',
+                onLast: true
+            }))
+           .pipe(browserSync.stream({match: '**/*.css'})),
+        //sass('src/css/scss/iestyles.scss', {
+        //        style: 'compressed', // expanded compressed
+        //        sourcemap: true,
+        //        loadPath: sassPaths
+        //    })
+        //    .pipe(errorHandler())
+        //    .pipe(sourcemaps.write('maps', {
+        //        includeContent: false,
+        //        sourceRoot: 'source'
+        //    }))
+        //    .pipe(gulp.dest('assets/css'))
+        //    .pipe(notify({
+        //        message: 'IE Styles task complete',
+        //        onLast: true
+        //    }))
+    ]
 });
 
-gulp.task('scripts', function() {
+
+gulp.task('pre-clean-scripts', function() {
+    return del('assets/scripts');
+});
+
+gulp.task('scripts', ['pre-clean-scripts'], function() {
     return gulp.src('src/js/**/*.js')
         .pipe(errorHandler())
         .pipe(jshint())
@@ -75,26 +97,38 @@ gulp.task('scripts', function() {
         .pipe(uglify())
         .pipe(sourcemaps.write(''))
         .pipe(gulp.dest('assets/js'))
-        .pipe(notify({ message: 'Scripts task complete' }));
+        .pipe(notify({
+            message: 'Scripts task complete',
+            onLast: true
+        }));
 });
 
-gulp.task('images', function() {
+
+gulp.task('pre-clean-images', function() {
+    return del('assets/img');
+});
+
+gulp.task('images', ['pre-clean-images'], function() {
     return gulp.src('src/img/**/*')
         .pipe(errorHandler())
-        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(imagemin({
+            progressive: true,
+            interlaced: true,
+            verbose: true
+        }))
         .pipe(gulp.dest('assets/img'))
-        .pipe(notify({ message: 'Images task complete' }));
+        .pipe(notify({
+            message: 'Images task complete',
+            onLast: true
+        }));
 });
 
-gulp.task('clean', function() {
-    return del(['assets/css', 'assets/js', 'assets/img']);
+
+gulp.task('pre-clean-bower-files', function() {
+   return del(['assets/lib']);
 });
 
-gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'scripts', 'images', 'bower-files' );
-});
- 
-gulp.task('pre-bower-files', function(){
+gulp.task('bower-files', ['pre-clean-bower-files'], function(){
     var filterJS = gulpFilter('**/*.js', { restore: true });
     var filterIMG = gulpFilter('**/*.png', '**/*.jpg', '**/*.gif', { restore: true });
 
@@ -103,30 +137,43 @@ gulp.task('pre-bower-files', function(){
         .pipe(jshint())
         .pipe(jshint.reporter(stylish))
         .pipe(mainBowerFiles())
-        .pipe(gulp.dest('assets/lib/delete')) //temporary folder, needed to trick it into loading in the .babelrc file, the files created are useless because if they are written with ES20015 and they won't really work unless they are ran through babel.
         .pipe(filterJS)
         .pipe(babel())
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
+        .pipe(notify({
+            message: 'bower-files [js files] task complete',
+            onLast: true
+        }))
         .pipe(gulp.dest('assets/lib'))
         .pipe(filterJS.restore)
         .pipe(filterIMG)
-        .pipe(gulp.dest('assets/img'));
+        .pipe(gulp.dest('assets/img'))
+        .pipe(notify({
+            message: 'bower-files [img files] task complete',
+            onLast: true
+        }));
 });
 
-gulp.task('bower-files', ['pre-bower-files'],  function() {
-    return del(['assets/lib/delete']); //delete temporary folder
-});
 
 gulp.task('watch', function() {
-  browserSync.init({
-    proxy: "http://localhost:8000/" //change to project's local url
-  });
+    browserSync.init({
+        proxy: "http://localhost:8000/" //change to project's local url
+    });
 
-  //files/folders to watch
-  gulp.watch('**/*.php').on('change', browserSync.reload);
-  gulp.watch('**/*.html').on('change', browserSync.reload);
-  gulp.watch('src/css/**/*.scss', ['styles', 'iestyles']);
-  gulp.watch('src/js/**/*.js', ['scripts']).on('change', browserSync.reload);
-  gulp.watch('src/img/**/*', ['images']).on('change', browserSync.reload);
+    //files/folders to watch
+    gulp.watch('**/*.php').on('change', browserSync.reload);
+    gulp.watch('**/*.html').on('change', browserSync.reload);
+    gulp.watch('src/css/**/*.scss', ['styles']);
+    gulp.watch('src/js/**/*.js', ['scripts']).on('change', browserSync.reload);
+    gulp.watch('src/img/**/*', ['images']).on('change', browserSync.reload);
+});
+
+
+gulp.task('clean', function() {
+    return del(['assets/css', 'assets/js', 'assets/img']);
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images', 'bower-files' );
 });
